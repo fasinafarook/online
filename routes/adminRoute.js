be72ce2652c909b18cc18a16fab49ce9cb0a794d
fasinafarook <fasinafarook786@gmail.com>
2024-03-02 16:52:31 +0530

    Initial commit

---
+++ routes/adminRoute.js
@@ -0,0 +1,126 @@
+const express=require('express');
+const admin_route=express();
+const config = require('../config/config');
+const auth = require('../middleware/adminAuth');
+const upload=require('../config/productMulter')
+const uploads=require('../config/categoryMulter')
+const bannerUplods = require('../config/bannerMulter')
+
+const errorHandler = require('../handler/errorHandler');
+const fs = require('fs')
+
+
+
+
+const adminsession = require('express-session');
+admin_route.use(adminsession({secret:config.secretSession,
+    resave: false,
+    saveUninitialized: true,}));
+
+
+
+const bodyparser=require('body-parser');
+admin_route.use(bodyparser.json());
+admin_route.use(bodyparser.urlencoded({extended:true}));
+
+admin_route.set('view engine','ejs');
+admin_route.set('views','./views/admin');
+
+admin_route.use(express.static('public'));
+
+
+const adminController = require('../controllers/adminController');
+const productController = require('../controllers/productController');
+const categoryController = require('../controllers/categoryController');
+const orderController = require('../controllers/orderController');
+const couponController = require('../controllers/coupenController');
+const offerController = require('../controllers/offerController');
+const bannerController = require('../controllers/bannerController');
+
+
+
+
+
+
+admin_route.get('/',auth.isLogout,adminController.loadLogin);
+admin_route.post('/',adminController.veryfyLogin);
+admin_route.get('/logout',auth.isLogin,adminController.adminLogout);
+admin_route.get('/home',auth.isLogin,adminController.loadDashboard);
+admin_route.post('/home',auth.isLogin,adminController.loadDashboard);
+
+
+
+admin_route.get('/report',auth.isLogin,adminController.reportDetails)
+
+
+
+admin_route.get('/users',auth.isLogin,adminController.loadUsers);
+admin_route.post('/users/toggleUserStatus/:userId',adminController.loadtoggleUserStatus)
+admin_route.get('/products',auth.isLogin,productController.loadProducts);
+admin_route.post('/products/toggleproductStatus/:productId',productController.loadtoggleProductStatus);
+admin_route.get('/addproducts',auth.isLogin,productController.loadAddProduct);
+admin_route.post('/addproducts',auth.isLogin,upload.array('image',4),productController.addProduct);
+admin_route.get('/category',auth.isLogin,categoryController.loadCategory);
+admin_route.post('/category/toggleCategoryStatus/:categoryId',categoryController.loadtoggleCategoryStatus);
+admin_route.get('/addcategory',auth.isLogin,categoryController.loadAddCategory);
+admin_route.post('/addcategory',uploads.single('image'),categoryController.addCategory);
+
+admin_route.post('/applycategoryOffer', categoryController.applycategoryOffer);
+admin_route.post('/removeCategoryOffer', categoryController.removeCategoryOffer);
+
+admin_route.post('/applyProductOffer', productController.applyProductOffer);
+admin_route.post('/removeProductOffer', productController.removeProductOffer);
+
+
+
+
+admin_route.get('/edit-product',auth.isLogin,productController.editProductLoad)
+admin_route.post('/edit-product',auth.isLogin,upload.array('image',4),productController.editProduct)
+admin_route.post('/edit-product/deleteimg',auth.isLogin,productController.deleteImg)
+
+admin_route.get('/edit-category',categoryController.editCategoryLoad);
+admin_route.post('/edit-category',uploads.single('image'),categoryController.updateCategory);
+
+
+admin_route.get('/order',auth.isLogin,orderController.orderManagement)
+admin_route.get('/orderStatus',auth.isLogin,orderController.orderStatus)
+admin_route.get('/vieworders',auth.isLogin,orderController.viewsorders)
+admin_route.post('/orderstatus',auth.isLogin,orderController.adminCancelOrder)
+
+
+admin_route.get('/coupon',auth.isLogin,couponController.couponManagement)
+admin_route.post('/coupon',auth.isLogin,couponController.addCoupons)
+admin_route.post('/updateCouponStatus',auth.isLogin,couponController.updateCouponStatus)
+admin_route.get('/editCoupon',auth.isLogin,couponController.loadEditCoupon)
+admin_route.post('/editCoupon',auth.isLogin,couponController.EditCoupon)
+
+admin_route.get('/offer',auth.isLogin,offerController.offerManagement)
+admin_route.post('/offer',auth.isLogin,offerController.addOffers)
+admin_route.post('/updateOfferStatus',auth.isLogin,offerController.updateOfferStatus)
+admin_route.get('/editOffer',auth.isLogin,offerController.loadEditOffer)
+admin_route.post('/editOffer',auth.isLogin,offerController.EditOffer)
+
+
+
+
+admin_route.get('/banner',auth.isLogin,bannerController.banneranagement)
+admin_route.get('/addbanner',auth.isLogin,bannerController.loadAddBanner)
+admin_route.post('/addbanner',auth.isLogin,bannerUplods.array('image',5),bannerController.addBanner)
+admin_route.post('/updateBannerStatus',auth.isLogin,bannerController.blockBanner)
+admin_route.get('/edit-banner',auth.isLogin,bannerController.loadEditBanner)
+admin_route.post('/edit-banner',auth.isLogin,bannerUplods.array('image',5),bannerController.editBanner)
+
+
+
+
+
+
+
+
+// admin_route.get('*',(req,res)=>{
+
+//     res.redirect('/admin/404page');
+
+// })
+
+module.exports = admin_route;
---
+++ routes/userRoute.js
@@ -0,0 +1,105 @@
+const express=require('express');
+const user_route=express();
+const config = require('../config/config');
+const auth = require('../middleware/auth');
+
+const session = require('express-session');
+// user_route.js
+user_route.use(session({
+    secret: config.secretSession,
+    resave: false,
+    saveUninitialized: true,
+}));
+
+
+
+const bodyparser=require('body-parser');
+user_route.use(bodyparser.json());
+user_route.use(bodyparser.urlencoded({extended:true}));
+
+ const path = require('path');
+
+user_route.set('view engine','ejs');
+user_route.set('views','./views/users');
+
+
+
+
+const userController = require('../controllers/userController');
+const productController = require('../controllers/productController');
+const wishlistController = require('../controllers/wishlistController');
+const orderController = require('../controllers/orderController');
+const cartController = require('../controllers/cartController');
+const couponController = require('../controllers/coupenController');
+
+
+
+
+
+
+user_route.get('/', userController.loadHome);
+
+user_route.get('/signup',auth.isLogout,userController.loadRegister);
+user_route.post('/signup',userController.insertUser);
+
+user_route.get('/login',auth.isLogout,userController.loginLoad);
+user_route.post('/login',auth.isLogout,userController.verifyLogin);
+
+user_route.get('/logout',auth.isLogin,userController.userLogout);
+
+user_route.get('/email-verified',userController.loadotp);
+user_route.post('/resend-otp',userController.updateLastResendTime, userController.resendOTP);
+user_route.post('/email-verified',userController.veryfyotp);
+
+user_route.get('/product',productController.loadUserProducts);
+user_route.get('/productDetails/:id',productController.productDetails);
+
+user_route.get('/wishlist',auth.isLogin,wishlistController.wishlistManagement)
+user_route.post('/addTowish',auth.isLogin,wishlistController.addTowish)
+user_route.post('/wishlist',auth.isLogin,wishlistController.wishManagementAddtowish)
+user_route.get('/deleteWishlistItem',auth.isLogin,wishlistController.deleteWishlistItem)
+
+user_route.get('/cart',auth.isLogin,cartController.cartManagement)
+user_route.get('/addtocart',auth.isLogin,cartController.addToCart)
+user_route.post('/cart',auth.isLogin,cartController.cartManagementAddtocart)
+user_route.get('/deleteCartitem',auth.isLogin,cartController.deleteCartitem)
+user_route.get('/quantitymanagement',auth.isLogin,cartController.quantityCheck)
+
+user_route.get('/payment-policy',auth.isLogin,userController.paymentPolicy)
+
+
+user_route.get('/qtycheck',auth.isLogin,cartController.qtycheck)
+user_route.get('/checkout',auth.isLogin,orderController.checkoutOrder)
+user_route.post('/checkout',auth.isLogin,orderController.checkoutaddress)
+
+user_route.post('/applycoupon',auth.isLogin,couponController.applycoupons)
+user_route.post('/couponCheck',auth.isLogin,couponController.pushingCoupon)
+user_route.post('/onlinepayment',auth.isLogin,orderController.onlinePay)
+
+user_route.post('/walletpayment',auth.isLogin,orderController. walletPayment)
+user_route.post('/proceedtoPayment',auth.isLogin,auth.isLogin,orderController.paymentManagement)
+
+
+user_route.get('/account',auth.isLogin,userController.accountManagment)
+user_route.get('/orderstatus',auth.isLogin,orderController.cancerlOrReturn)
+user_route.post('/orderstatus',auth.isLogin,orderController.userCancelOrder)
+user_route.get('/invoice',auth.isLogin,orderController.invoiceDownload)
+
+user_route.get('/editAddress',auth.isLogin,userController.showeditaddress)
+user_route.post('/editAddress',auth.isLogin,userController.editaddress)
+user_route.get('/editProfile',auth.isLogin,userController.profileEdit)
+user_route.post('/addaddress',auth.isLogin,userController.addaddress)
+user_route.post('/changePassword',auth.isLogin,userController.changePassword)
+user_route.get('/deleteAddress',auth.isLogin,userController.deleteAddress)
+
+
+
+
+
+
+
+
+module.exports = user_route;
+
+
+
